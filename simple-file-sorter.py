a4a3f0fc7155362b7efaacfa90653b0cc664d1bc
Afad Fath Musyarof Halim <afadfath03@gmail.com>
2025-03-23 06:15:44 +0700

    Add Exclude file type and improve folder structure

--- simple-file-sorter.py
+++ simple-file-sorter.py
@@ -2,14 +2,26 @@ import os
 import shutil
 import string
 
+exclude_extensions = input("Masukkan tipe file yang ingin di-exclude (pisahkan dengan koma, contoh: 'py,txt'): ")
+EXCLUDE_EXTENSIONS = [ext.strip().lower() for ext in exclude_extensions.split(',')]
+
 for item in os.listdir():
     if os.path.isfile(item):
-        first_char = item[0].upper()
+        file_ext = os.path.splitext(item)[1].lower().lstrip('.')
+        if file_ext in EXCLUDE_EXTENSIONS:
+            print(f"File {item} di-exclude karena ekstensinya ({file_ext})")
+            continue
+
+        first_char = item[0]
         
-        if first_char in string.ascii_uppercase:
-            folder_name = first_char
+        if first_char.isdigit():
+            folder_name = '#'
+        elif first_char in string.punctuation:
+            folder_name = '-'
+        elif first_char in string.ascii_letters:
+            folder_name = first_char.upper()
         else:
-            folder_name = 'Unsupported'
+            folder_name = 'Unsorted'
         
         if not os.path.exists(folder_name):
             os.mkdir(folder_name)
